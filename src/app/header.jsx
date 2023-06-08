c3ad7b4acd8db66de823ccbe3132e89b72793963
Priya313neosoft <priyabhagat313@gmail.com>
2023-06-08 12:08:44 +0530

    header

---
+++ src/app/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from "next/link";
+
+function Header() {
+  return (
+    <div className='header'>
+         <div>
+        <h2>Todo.</h2>
+      </div>
+        
+        
+        header</div>
+  )
+}
+
+export default Header
